3bb239df51cc1464ccee89df86a30ebc3b165c15
andypaxo <andy@andypaxo.net>
2010-08-01 12:36:57 -0600

    Attempting to sort out capitilization problems in Git. Windows client / *NIX server not great

---
+++ Chartis/DataAccess/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using ChartisDomain;
+
+namespace Chartis.DataAccess
+{
+    public class Repository : DbContext
+    {
+        public DbSet<Sprint> Sprints { get; set; }
+        public DbSet<Story> Stories { get; set; }
+    }
+}
